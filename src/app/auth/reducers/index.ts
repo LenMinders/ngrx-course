a669931141e98ca759033e6c1b1d35962144f6b8
Len Minders <len.minders@standardbank.co.za>
2021-07-18 17:14:11 +0200

    store user on login

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -2,14 +2,25 @@ import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
-  MetaReducer
-} from '@ngrx/store';
+  MetaReducer,
+  on,
+} from "@ngrx/store";
+import { AuthActions } from "../action-types";
+import { User } from "../model/user.model";
 
-export interface State {
-
+export interface AuthState {
+  user: User;
 }
 
-export const reducers: ActionReducerMap<State> = {
-
+export const initialAuthState: AuthState = {
+  user: null,
 };
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return { user: action.user };
+  })
+);
