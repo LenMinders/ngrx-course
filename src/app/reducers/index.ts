d3cff607ee468f558c0a14ab66369fe1e5de90a3
Len Minders <len.minders@standardbank.co.za>
2021-07-21 07:21:56 +0200

    implement ngrx debugging

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,3 +1,4 @@
+import { routerReducer } from '@ngrx/router-store';
 import {
   ActionReducer,
   ActionReducerMap,
@@ -12,8 +13,16 @@ export interface State {
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  router: routerReducer
 };
 
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state before', state);
+    console.log('action', action);
+    
+    return reducer(state, action);
+  };
+}
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [logger] : [];
