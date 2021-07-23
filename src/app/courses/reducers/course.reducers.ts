a1f14f05b86d987129a1228f7c717033e5e818be
Len Minders <len.minders@standardbank.co.za>
2021-07-23 17:40:56 +0200

    display courses from store

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -3,16 +3,17 @@ import { createReducer, on } from "@ngrx/store";
 import { CourseActions } from "../action-types";
 import { Course } from "../model/course";
 
-
-export interface CourseState extends EntityState<Course>{
-
-}
+export interface CoursesState extends EntityState<Course> {}
 
 export const adapter = createEntityAdapter<Course>();
 
 export const initialCoursesState = adapter.getInitialState();
 
 export const coursesReducer = createReducer(
-    initialCoursesState,
-    on(CourseActions.allCoursesLoaded, (state, action) => adapter.addAll(action.courses, state))
+  initialCoursesState,
+  on(CourseActions.allCoursesLoaded, (state, action) =>
+    adapter.addAll(action.courses, state)
+  )
 );
+
+export const { selectAll } = adapter.getSelectors();
