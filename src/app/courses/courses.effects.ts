b8574913da60d6fe0140e1fdf56c13b661e31618
Len Minders <len.minders@standardbank.co.za>
2021-08-15 16:34:05 +0200

    edit course implementation

--- src/app/courses/courses.effects.ts
+++ src/app/courses/courses.effects.ts
@@ -7,17 +7,30 @@ import { CoursesHttpService } from "./services/courses-http.service";
 
 @Injectable()
 export class CoursesEffects {
+  loadCourses = createEffect(() =>
+    this.actions$.pipe(
+      ofType(CourseActions.loadAllCourses),
+      concatMap((action) => this.coursesHttpService.findAllCourses()),
+      map((courses) => allCoursesLoaded({ courses }))
+    )
+  );
 
-    loadCourses = createEffect(
-        () => this.actions$.pipe(
-            ofType(CourseActions.loadAllCourses),
-            concatMap(action => this.coursesHttpService.findAllCourses()),
-            map(courses => allCoursesLoaded({courses}))
+  saveCourse$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(CourseActions.courseUpdated),
+        concatMap((action) =>
+          this.coursesHttpService.saveCourse(
+            action.update.id,
+            action.update.changes
+          )
         )
-    );
+      ),
+    { dispatch: false }
+  );
 
-    constructor(private actions$: Actions, private coursesHttpService: CoursesHttpService) {
-
-    }
-
+  constructor(
+    private actions$: Actions,
+    private coursesHttpService: CoursesHttpService
+  ) {}
 }
