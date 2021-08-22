c13ec740469d908aea7a2d03fdc94ce32dc9533b
Len Minders <len.minders@standardbank.co.za>
2021-08-22 16:56:23 +0200

    implement add course using ngrx data

--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,53 +1,51 @@
-import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {Course} from '../model/course';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Observable} from 'rxjs';
-import { CourseEntityService } from '../services/course-entity.service';
+import { Component, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { Course } from "../model/course";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
+import { CourseEntityService } from "../services/course-entity.service";
 
 @Component({
-  selector: 'course-dialog',
-  templateUrl: './edit-course-dialog.component.html',
-  styleUrls: ['./edit-course-dialog.component.css']
+  selector: "course-dialog",
+  templateUrl: "./edit-course-dialog.component.html",
+  styleUrls: ["./edit-course-dialog.component.css"],
 })
 export class EditCourseDialogComponent {
-
   form: FormGroup;
 
   dialogTitle: string;
 
   course: Course;
 
-  mode: 'create' | 'update';
+  mode: "create" | "update";
 
-  loading$:Observable<boolean>;
+  loading$: Observable<boolean>;
 
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<EditCourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data,
-    private coursesService: CourseEntityService) {
-
+    private coursesService: CourseEntityService
+  ) {
     this.dialogTitle = data.dialogTitle;
     this.course = data.course;
     this.mode = data.mode;
 
     const formControls = {
-      description: ['', Validators.required],
-      category: ['', Validators.required],
-      longDescription: ['', Validators.required],
-      promo: ['', []]
+      description: ["", Validators.required],
+      category: ["", Validators.required],
+      longDescription: ["", Validators.required],
+      promo: ["", []],
     };
 
-    if (this.mode == 'update') {
+    if (this.mode == "update") {
       this.form = this.fb.group(formControls);
-      this.form.patchValue({...data.course});
-    }
-    else if (this.mode == 'create') {
+      this.form.patchValue({ ...data.course });
+    } else if (this.mode == "create") {
       this.form = this.fb.group({
         ...formControls,
-        url: ['', Validators.required],
-        iconUrl: ['', Validators.required]
+        url: ["", Validators.required],
+        iconUrl: ["", Validators.required],
       });
     }
   }
@@ -57,18 +55,18 @@ export class EditCourseDialogComponent {
   }
 
   onSave() {
-
     const course: Course = {
       ...this.course,
-      ...this.form.value
+      ...this.form.value,
     };
 
-    if(this.mode == 'update') {
+    if (this.mode == "update") {
       this.coursesService.update(course);
       this.dialogRef.close();
+    } else if (this.mode == "create") {
+      this.coursesService.add(course).subscribe((newCourse) => {
+        this.dialogRef.close();
+      });
     }
-
   }
-
-
 }
